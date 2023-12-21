6212a71fd1d6042320679e57b0cb03795a5ac945
Sergei Ananev <9852407799s@gmail.com>
2023-12-21 22:00:12 +0100

    feat: deploy on GitHub Pages

--- src/redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { postReducer } from './reducer';
-
-export const store = configureStore({
-  reducer: postReducer,
-});
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
