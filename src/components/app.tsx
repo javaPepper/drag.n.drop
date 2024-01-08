938d5930e07d280553f2763a866f3daa22c9e3a5
Sergei Ananev <9852407799s@gmail.com>
2024-01-08 23:43:47 +0100

    refact: delete comments, add css variables

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,22 +1,13 @@
-// import Footer from './footer';
 import NavBar from './nav-bar';
 import UsersList from './users-list';
 
 export default function App() {
   return (
     <div className='container'>
-      {/* <header>
-        <div className="nav-container">
-          <NavBar />
-        </div>
-      </header> */}
       <main className="main-container">
         <UsersList />
         <NavBar />
       </main>
-      {/* <footer>
-        <Footer />
-      </footer> */}
     </div>
   );
 }
