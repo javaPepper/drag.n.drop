60db78dc8a9980eb0c07fcb279d9e1ff4451e240
Sergei Ananev <9852407799s@gmail.com>
2024-01-28 06:22:11 +0100

    feat: implement react-beautiful-dnd

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,49 @@
+import { useAppDispatch, useAppSelector } from '../hooks';
+import { fetchUsers } from '../redux/actions';
 import NavBar from './nav-bar';
 import UsersList from './users-list';
+import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd';
 
 export default function App() {
+
+  const dispatch = useAppDispatch();
+  const users = useAppSelector((state) => state.users);
+
+  const onDragEndHandler = (result: DropResult) => {
+    if(!result.destination) {
+      return null;
+    }
+    if (result.destination.index === result.source.index) {
+      return null;
+    }
+    const items = Array.from(users);
+    const [reorderedItems] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItems);
+    dispatch(fetchUsers(items));
+  };
+
   return (
     <div className='container'>
       <main className="main-container">
-        <UsersList />
+        <DragDropContext
+          onDragEnd={onDragEndHandler}
+        >
+          <Droppable
+            droppableId='droppable'
+          >
+            {(provided) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                <UsersList
+                  users={users}
+                />
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
         <NavBar />
       </main>
     </div>
--- src/components/user.tsx
+++ src/components/user.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import GetPostsButton from './get-posts-button';
+import { Draggable } from 'react-beautiful-dnd';
+import { UserType } from '../types/user';
 
 type UserProps = {
-  id: number;
-  name: string;
-  email: string;
-  phone: string;
-};
+  index: number;
+  props: UserType;
+}
 
-export default function User({ id, name, email, phone }: UserProps) {
+export default function User({ index, props }: UserProps) {
+  const {...properties} = props;
+
   const [isMouseOver, setMouseOver] = useState<boolean>(false);
   const [isMouseLeave, setMouseLeave] = useState<boolean>(false);
 
@@ -25,13 +27,32 @@ export default function User({ id, name, email, phone }: UserProps) {
           setMouseLeave(false);
         }}
       >
-        <div className="visually-hidden">{id}</div>
-        <ul className="user-description-list">
-          <li className="name">{name}</li>
-          <li className="email">{email}</li>
-          <li className="phone">{phone}</li>
-        </ul>
-        {isMouseOver && !isMouseLeave && <GetPostsButton id={id} />}
+        <div className="visually-hidden">{properties.id}</div>
+        <Draggable
+          draggableId={`${index}`}
+          index={index}
+          key={index}
+        >
+          {(provided) => (
+            <div
+              className='draggable'
+              ref={provided.innerRef}
+              {...provided.dragHandleProps}
+              {...provided.draggableProps}
+            >
+              <ul className="user-description-list">
+                <li className="name">{properties.name}</li>
+                <li className="email">{properties.email}</li>
+                <li className="phone">{properties.phone}</li>
+              </ul>
+            </div>
+          )}
+        </Draggable>
+        {isMouseOver &&
+        !isMouseLeave &&
+        <GetPostsButton
+          id={properties.id}
+        />}
       </div>
     </article>
   );
